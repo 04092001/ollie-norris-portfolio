30a7be5e41ba6f07ddc71bd7f6d4fdd0672a79a9
Freddie Heath <fheath090401@gmail.com>
2024-05-06 19:06:46 +0100

    Added styles and made template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./public/**/*.{html,js}/"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./public/**/*.{html,js}/'],
+    theme: {
+        extend: {
+            fontFamily: {
+                bodoni: ['"Bodoni Moda"'],
+                ...defaultTheme.fontFamily.sans,
+            },
+        },
+        colors: {
+            onRed: '#a800000',
+            onRedTwo: '#731010',
+            onBlack: '#1f1f1f',
+            onDarkGrey: '#707070',
+            onLightGrey: '#d4d4d4',
+        },
+    },
+    plugins: [],
+}
